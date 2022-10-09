dfaa182a64670fbee2e8c5d2339b93cdce55ace7
Jędrzej Głowaczewski <jedrzej.glowaczewski@stxnext.pl>
2022-10-09 17:57:42 +0200

    Fix Leaflet icons path (it doesn't support webpack by deafult)

--- src/components/MapDemo.vue
+++ src/components/MapDemo.vue
@@ -9,6 +9,14 @@ import "leaflet/dist/leaflet.css";
 export default {
   name: "MapDemo",
   async mounted() {
+    // Fix icons path (leaflet doesn't support webpack by deafult)
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    });
+
     var map = L.map("map").setView([51.505, -0.09], 13);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
