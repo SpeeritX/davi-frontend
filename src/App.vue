6af39f2b040107b9981970eb23dd9feb19990b03
Jędrzej Głowaczewski <jedrzej.glowaczewski@stxnext.pl>
2022-10-29 15:31:02 +0200

    Update layout

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,13 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,7 +1,12 @@
 <template>
-  <div id="container">
-    <MapDemo />
-    <ChartDemo />
+  <div id="main-container">
+    <div id="main-content">
+      <MapDemo />
+    </div>
+    <div id="sidebar">
+      <!-- TODO: Minimap -->
+      <ChartDemo id="charts" />
+    </div>
   </div>
 </template>
 
@@ -18,19 +23,37 @@ export default {
 };
 </script>
 
-<style>
-#container {
+<style scoped>
+#main-container {
+  height: calc(100% - 2rem);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   display: flex;
   width: 100%;
   max-width: calc(100% - 20rem);
-  padding: 0rem 10rem;
+  padding: 1rem;
   align-items: center;
+  flex-direction: row;
+}
+
+#main-content {
+  height: 100%;
+  flex: 3;
+}
+
+#sidebar {
+  height: 100%;
+  flex: 1;
+  display: flex;
   flex-direction: column;
+}
+
+#minimap,
+#charts {
+  flex: 1;
+  height: 100%;
 }
 </style>
